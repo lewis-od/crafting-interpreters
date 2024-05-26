5ed3d0290ecf193b7b828be52de5f493d8322433
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-05-26 18:32:03 +0100

    feat: add AstPrinter for debugging

--- jlox/src/main/java/uk/co/lewisod/lox/Token.java
+++ jlox/src/main/java/uk/co/lewisod/lox/Token.java
@@ -1,10 +1,10 @@
 package uk.co.lewisod.lox;
 
 public class Token {
-    private final TokenType type;
-    private final String lexeme;
-    private final Object literal;
-    private final int line;
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
 
     public Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
