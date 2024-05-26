3e13d36ce37fd056ff36f7aa7cab0848ee779c26
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-05-26 22:28:20 +0100

    feat: initial interpreter implementation

---
+++ jlox/src/main/java/uk/co/lewisod/lox/Interpreter.java
@@ -0,0 +1,127 @@
+package uk.co.lewisod.lox;
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+    public void interpret(Expr expression) {
+        try {
+            var value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private static String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            var text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        var left = evaluate(expr.left);
+        var right = evaluate(expr.right);
+
+        return switch (expr.operator.type) {
+            case MINUS -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left - (double) right;
+            }
+            case SLASH -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left / (double) right;
+            }
+            case STAR -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left * (double) right;
+            }
+            case PLUS -> {
+                if (left instanceof Double first && right instanceof Double second) {
+                    yield first + second;
+                }
+
+                if (left instanceof String first && right instanceof String second) {
+                    yield first + second;
+                }
+
+                throw new RuntimeError(expr.operator, "Operands must both be numbers or strings");
+            }
+            case GREATER -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left > (double) right;
+            }
+            case GREATER_EQUAL -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left >= (double) right;
+            }
+            case LESS -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left < (double) right;
+            }
+            case LESS_EQUAL -> {
+                checkNumberOperand(expr.operator, left, right);
+                yield (double) left <= (double) right;
+            }
+            case BANG_EQUAL -> !isEqual(left, right);
+            case EQUAL_EQUAL -> isEqual(left, right);
+            default -> throw new IllegalStateException("Unknown binary expression encountered");
+        };
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        var right = evaluate(expr.right);
+
+        return switch (expr.operator.type) {
+            case MINUS -> {
+                checkNumberOperand(expr.operator, right);
+                yield -(double) right;
+            }
+            case BANG -> !isTruthy(right);
+            default -> throw new IllegalStateException("Unknown unary expression encountered");
+        };
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private static boolean isTruthy(Object value) {
+        if (value == null) return false;
+        if (value instanceof  Boolean) return (boolean)value;
+        return true;
+    }
+
+    private static boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+
+    private static void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number");
+    }
+
+    private static void checkNumberOperand(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers");
+    }
+}
--- jlox/src/main/java/uk/co/lewisod/lox/Lox.java
+++ jlox/src/main/java/uk/co/lewisod/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     private static boolean hadError = false;
+    private static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -30,6 +32,10 @@ public class Lox {
 
         if (hadError) {
             System.exit(65);
+        }
+
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -54,8 +60,7 @@ public class Lox {
 
         if (hadError) return;
 
-        var printer = new AstPrinter();
-        System.out.println(printer.print(expression));
+        interpreter.interpret(expression);
     }
 
     public static void error(Token token, String message) {
@@ -73,5 +78,10 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    public static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
