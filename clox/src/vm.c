f6ecd5a4b19f9db3edccfd779f1ecf3a5821557b
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-06-27 21:41:05 +0100

    feat: implement basic stack

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -5,7 +5,13 @@
 
 VM vm;
 
-void initVM() {}
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
+void initVM() {
+  resetStack();
+}
 
 void freeVM() {}
 
@@ -15,6 +21,13 @@ InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -22,11 +35,12 @@ InterpretResult run() {
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_RETURN:
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
     }
   }
@@ -39,4 +53,14 @@ InterpretResult interpret(Chunk* chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
