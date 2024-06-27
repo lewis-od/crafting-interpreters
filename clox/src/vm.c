6b867cf13301e0e02e6f8111fefa95f580baaa84
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-06-27 21:52:13 +0100

    feat: implement binary operations

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -18,6 +18,12 @@ void freeVM() {}
 InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do {                \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false);
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -41,6 +47,18 @@ InterpretResult run() {
       case OP_NEGATE:
         push(-pop());
         break;
+      case OP_ADD:
+        BINARY_OP(+);
+        break;
+      case OP_SUBTRACT:
+        BINARY_OP(-);
+        break;
+      case OP_MULTIPLY:
+        BINARY_OP(*);
+        break;
+      case OP_DIVIDE:
+        BINARY_OP(/);
+        break;
       case OP_RETURN:
         printValue(pop());
         printf("\n");
@@ -50,6 +68,7 @@ InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
