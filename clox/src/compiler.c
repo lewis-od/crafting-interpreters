3c5f4fda7155ae2744d6c9fb47d36d9719724a6e
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-09-29 14:16:55 +0100

    feat(clox): implement for loops

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -533,6 +533,59 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void forStatement() {
+  beginScope();
+  consume(TOKEN_LEFT_PAREN, "Excpect '(' after 'for'.");
+  if (match(TOKEN_SEMICOLON)) {
+    // No initialiser clause
+  } else if (match(TOKEN_VAR)) {
+    varDeclaration();
+  } else {
+    expressionStatement();
+  }
+
+  int loopStart = currentChunk()->count;
+
+  int exitJump = -1;
+  if (!match(TOKEN_SEMICOLON)) {
+    // There is a condition clause
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+    // Exit loop if condition is false
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);  // Pop condition value off stack
+  }
+
+  if (!match(TOKEN_RIGHT_PAREN)) {
+    // There is an increment clause
+
+    // Jump over increment clause to body of loop
+    int bodyJump = emitJump(OP_JUMP);
+
+    int incrementStart = currentChunk()->count;
+    expression();
+    emitByte(OP_POP);  // Discard return value of increment clause
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    emitLoop(loopStart);
+    // Make sure the loop jumps back to the increment clause, rather than the
+    // start of the body
+    loopStart = incrementStart;
+    patchJump(bodyJump);
+  }
+
+  statement();
+  emitLoop(loopStart);
+
+  if (exitJump != -1) {
+    patchJump(exitJump);
+    emitByte(OP_POP);  // Pop condition value
+  }
+
+  endScope();
+}
+
 static void ifStatement() {
   consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
   expression();
@@ -616,6 +669,8 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_FOR)) {
+    forStatement();
   } else if (match(TOKEN_IF)) {
     ifStatement();
   } else if (match(TOKEN_WHILE)) {
