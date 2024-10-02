3440e74aa9a2ec6bccce5bd1f27a203a4eb3806f
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-10-02 22:35:22 +0100

    feat(clox): begin implementing closures using upvalues

--- clox/src/object.h
+++ clox/src/object.h
@@ -20,9 +20,10 @@
 
 typedef enum {
   OBJ_CLOSURE,
-  OBJ_STRING,
-  OBJ_NATIVE,
   OBJ_FUNCTION,
+  OBJ_NATIVE,
+  OBJ_STRING,
+  OBJ_UPVALUE,
 } ObjType;
 
 struct Obj {
@@ -33,6 +34,7 @@ struct Obj {
 typedef struct {
   Obj obj;
   int arity;
+  int upvalueCount;
   Chunk chunk;
   ObjString* name;
 } ObjFunction;
@@ -53,7 +55,14 @@ struct ObjString {
 
 typedef struct {
   Obj obj;
+  Value* location;
+} ObjUpvalue;
+
+typedef struct {
+  Obj obj;
   ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
 } ObjClosure;
 
 ObjClosure* newClosure(ObjFunction* function);
@@ -61,6 +70,7 @@ ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
+ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
