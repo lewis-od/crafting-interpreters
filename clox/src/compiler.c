9351b106109732e0598bd64bfc8b8d0eef3f5215
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-11-22 16:41:16 +0000

    feat(clox): begin implementing gc - mark phase

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -6,6 +6,7 @@
 #include "compiler.h"
 #include "object.h"
 #include "scanner.h"
+#include "memory.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -861,4 +862,12 @@ ObjFunction* compile(const char* source) {
 
   ObjFunction* function = endCompiler();
   return parser.hadError ? NULL : function;
+}
+
+void markCompilerRoots() {
+  Compiler* compiler = current;
+  while (compiler != NULL) {
+    markObject((Obj*)compiler->function);
+    compiler = compiler->enclosing;
+  }
 }
--- clox/src/table.h
+++ clox/src/table.h
@@ -27,5 +27,6 @@ ObjString* tableFindString(Table* table,
                            const char* chars,
                            int length,
                            uint32_t hash);
+void markTable(Table* table);
 
 #endif
